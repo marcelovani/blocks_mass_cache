aebdb522483bebc43e46b42261e3c7dfbe5ec204
Marcelo Vani <marcellovani@yahoo.co.uk>
2015-01-12 21:59:41 +0000

    Reformatting code

--- blocks_mass_cache_ui/blocks_mass_cache_ui.js
+++ blocks_mass_cache_ui/blocks_mass_cache_ui.js
@@ -1,7 +1,7 @@
 (function ($) {
-Drupal.behaviors.blocksMassCache = {
-  attach: function (context) {
-    jQuery("table#blocks input:checkbox").click(function () {
+  Drupal.behaviors.blocksMassCache = {
+    attach: function (context) {
+      jQuery("table#blocks input:checkbox").click(function () {
         var block_name = (jQuery(this).parents("tr").find("td").attr("id"));
         var class_name = jQuery(this).attr("class");
         var cache_type = class_name.match(/rid-(.*?)\s/);
@@ -12,26 +12,26 @@ Drupal.behaviors.blocksMassCache = {
         // Class.
         class_name = "rid-" + cache_type;
 
-        switch(cache_type) {
-            case "-1":
-            case "-2":
-            case "8":
-                jQuery("table#blocks" + " ." + block_name + " input:checkbox:not(." + class_name + ")").attr('checked', '');
-                jQuery("table#blocks" + " ." + block_name + " input:checkbox." + class_name).attr('checked', 'checked');
-                break;
-            case "1":
-                jQuery("table#blocks" + " ." + block_name + " input:checkbox:not(." + class_name + ", .rid-4)").attr('checked', '');
-                break;
-            case "2":
-                jQuery("table#blocks" + " ." + block_name + " input:checkbox:not(." + class_name + ", .rid-4)").attr('checked', '');
-                break;
-            case "4":
-                jQuery("table#blocks" + " ." + block_name + " input:checkbox:not(." + class_name + ", .rid-1, .rid-2)").attr('checked', '');
-                break;
-            default:
+        switch (cache_type) {
+          case "-1":
+          case "-2":
+          case "8":
+            jQuery("table#blocks" + " ." + block_name + " input:checkbox:not(." + class_name + ")").attr('checked', '');
+            jQuery("table#blocks" + " ." + block_name + " input:checkbox." + class_name).attr('checked', 'checked');
+            break;
+          case "1":
+            jQuery("table#blocks" + " ." + block_name + " input:checkbox:not(." + class_name + ", .rid-4)").attr('checked', '');
+            break;
+          case "2":
+            jQuery("table#blocks" + " ." + block_name + " input:checkbox:not(." + class_name + ", .rid-4)").attr('checked', '');
+            break;
+          case "4":
+            jQuery("table#blocks" + " ." + block_name + " input:checkbox:not(." + class_name + ", .rid-1, .rid-2)").attr('checked', '');
+            break;
+          default:
         }
-    });
-  }
-};
+      });
+    }
+  };
 
 })(jQuery);
